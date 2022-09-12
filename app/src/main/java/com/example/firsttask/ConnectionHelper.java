bea77d33bd1d68dbf90dce84bc7c9f31804c28e1
temamironovonline <a.sergeichev03@mail.ru>
2022-09-12 10:47:19 +0300

    create connection class to sql

---
+++ app/src/main/java/com/example/firsttask/ConnectionHelper.java
@@ -0,0 +1,40 @@
+package com.example.firsttask;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+
+    String userName, userPassword, ip, port, dataBase;
+
+    public Connection connectionClass()
+    {
+        ip = "ngknn.ru";
+        dataBase = "";
+        userName = "31П";
+        userPassword = "12357";
+        port = "1433";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Connection connection = null;
+        String connectionURL = null;
+
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + dataBase + ";user=" + userName + ";password=" + userPassword + ";";
+            connection = DriverManager.getConnection(connectionURL);
+
+        }
+        catch (Exception ex)
+        {
+            Log.e("Error", ex.getMessage());
+        }
+        return connection;
+    }
+}
